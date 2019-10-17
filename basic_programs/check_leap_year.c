592fec61cb4e5aaa18439a0d2c42b7d8b771deb1
manjeetharyani <43161653+manjeetharyani@users.noreply.github.com>
2019-10-17 19:29:14 +0530

    Update check_leap_year.c

--- basic_programs/check_leap_year.c
+++ basic_programs/check_leap_year.c
@@ -3,7 +3,7 @@ int main()
 {
     int y;
 
-    printf("Enter year: ");
+    printf("Enter year to be checked : ");
     scanf("%d",&y);
 
     if(y % 4 == 0)
@@ -12,15 +12,15 @@ int main()
         if( y % 100 == 0)
         {
             if ( y % 400 == 0)
-                printf("%d is a Leap Year", y);
+                printf("%d is a LEAP YEAR", y);
             else
-                printf("%d is not a Leap Year", y);
+                printf("%d is not a LEAP YEAR", y);
         }
         else
-            printf("%d is a Leap Year", y );
+            printf("%d is a LEAP YEAR", y );
     }
     else
-        printf("%d is not a Leap Year", y);
+        printf("%d is not a LEAP YEAR", y);
 
     return 0;
 }
